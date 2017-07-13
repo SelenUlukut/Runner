ad5631a39d2eb7f854d11f17c4af60c56ba816f0
Selen <selen.demiryas@gmail.com>
2017-07-13 16:40:02 +0300

    Karakter kontrol, kamera kontrol, yol prefab

---
+++ Assets/Scripts/CamreraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamreraController : MonoBehaviour {
+
+    private Transform lookAt;
+    private Vector3 offset;
+    private Vector3 moveVec;
+
+    // Use this for initialization
+    void Start () {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - lookAt.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVec = lookAt.position + offset;
+        moveVec.x = 0;
+        moveVec.y = transform.position.y;
+        transform.position = moveVec;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private CharacterController controller;
+    public float speed=5.0f;
+    private Vector3 moveVector;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = Vector3.forward * speed;
+        moveVector.x += Input.GetAxisRaw("Horizontal") * speed;
+
+        controller.Move(moveVector * Time.deltaTime );
+
+	}
+}
