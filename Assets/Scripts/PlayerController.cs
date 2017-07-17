dd0dfcc5c713aae074f8d405b52d6e9545d5189b
Selen <selen.demiryas@gmail.com>
2017-07-17 11:55:20 +0300

    Player control, road spawn, obstacle spawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private CharacterController controller;
-    public float speed=5.0f;
+    public float speed=0;
+    public float cont;
     private Vector3 moveVector;
 	// Use this for initialization
 	void Start () {
+        speed = 1000f;
         controller = GetComponent<CharacterController>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        float h = 5f * Input.GetAxisRaw("Horizontal");
+        transform.Rotate(0, h, 0);
+     
+        while (transform.rotation.y > 0.4f)
+        {
+            transform.Rotate(0,-0.01f,0);
+        }
+        while (transform.rotation.y < -0.4f)
+        {
+            transform.Rotate(0, 0.01f, 0);
+        }
+
+
         moveVector = Vector3.forward * speed;
-        moveVector.x += Input.GetAxisRaw("Horizontal") * speed;
+        moveVector = transform.rotation * moveVector;
+        controller.SimpleMove(moveVector * Time.deltaTime);
 
-        controller.Move(moveVector * Time.deltaTime );
 
-	}
+
+    }
 }
+//top niye çok ileriye gidiyo en başta?
